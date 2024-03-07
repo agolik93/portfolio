1e7a30e0f7c1099bfe113addf2cbc69ff5dd578b
agolik93 <augustin.aljinovic@gmail.com>
2024-03-07 23:38:36 +0100

    2

--- src/portfolio.js
+++ src/portfolio.js
@@ -18,13 +18,16 @@ const about = {
 
 const projects = [
   {
+    name: "Coming soon",
+  },
+  /*  {
     name: "Project 1",
     description:
       "Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam",
     stack: ["SASS", "TypeScript", "React"],
     sourceCode: "https://github.com",
     livePreview: "https://github.com",
-  },
+  }, */
 ];
 
 const skills = [
