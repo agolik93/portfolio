7c880db504d2ae26f7d1997010c87ca448b077c0
agolik93 <augustin.aljinovic@gmail.com>
2024-03-07 23:32:17 +0100

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,44 @@
+const header = {
+  homepage: "/",
+  title: "AA.",
+};
+
+const about = {
+  name: "Augustin Aljinović",
+  role: "Front End Developer",
+  description:
+    "My name is Augustin Aljinović, and I am a passionate and motivated beginner in the world of frontend development. I began my journey into programming about a year ago with the goal of turning my passion for technology into a career. Through continuous learning and hands-on experience, I have mastered the basics of key technologies necessary for success as a frontend developer.I obtained a significant portion of my formal frontend education by attending courses, such as Algebra's React JS and Digitalna Dalmacija's frontend courses. However, an important part of my learning journey also involved independent project work and online courses on platforms like Udemy.I am ready for the next step in my career, where I can apply my knowledge and skills in practice and continue to grow and learn as part of a dynamic team.",
+  resume:
+    "https://drive.google.com/file/d/1oZq09o7uwZgN4uTerpJ1KV2EYYVP3Kyl/view?usp=drive_link",
+  social: {
+    linkedin: "https://hr.linkedin.com/in/augustin-aljinovic-691064203",
+    github: "https://github.com/agolik93",
+  },
+};
+
+const projects = [
+  {
+    name: "Project 1",
+    description:
+      "Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam",
+    stack: ["SASS", "TypeScript", "React"],
+    sourceCode: "https://github.com",
+    livePreview: "https://github.com",
+  },
+];
+
+const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Redux",
+  "Git",
+];
+
+const contact = {
+  email: "augustin.aljinovic@gmail.com",
+};
+
+export { header, about, projects, skills, contact };
